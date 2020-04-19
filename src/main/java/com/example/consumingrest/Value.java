c32412735303a2034f6b47b6f6071aac85d74563
Dillon86 <Dillon86@163.com>
2020-04-19 12:13:44 +0800

    init files

---
+++ src/main/java/com/example/consumingrest/Value.java
@@ -0,0 +1,36 @@
+package com.example.consumingrest;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Value {
+    private long  id;
+    private String quote;
+
+    public Value() {
+    }
+
+    @Override
+    public String toString() {
+        return "Value{" +
+                "id=" + id +
+                ", quote='" + quote + '\'' +
+                '}';
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getQuote() {
+        return quote;
+    }
+
+    public void setQuote(String quote) {
+        this.quote = quote;
+    }
+}
